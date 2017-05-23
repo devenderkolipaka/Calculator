796d9d4465430eaa2dd2392dd210e28df117af8b
iSTAR <iSTAR@192.168.100.17>
2017-05-23 18:11:46 +0530

    successfully completed basic calculator .... 23/05/17 devenderkolipaka jsp2 batch

--- src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
+++ src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
@@ -1,49 +1,99 @@
 package com.onlinecalculator.calculator.calctypes;
 import java.util.Scanner;
-
-public class BasicCalc
+    class Calc1
 {
-	
-	public BasicCalc()
+int a,b,c,res;
+
+public calc1()
         {
-		
+	a=0;
+	b=0;
+	c=0;
+	res=0;
 	}
 	
 	public int menu()
         {
+		System.out.println("1 for addition");
+		System.out.println("2 for substraction");
+    	System.out.println("3 for mulitiplication");
+    	System.out.println("4 for division");
+    	System.out.println("enter your choice");
+    	Scanner S=new Scanner(System.in);
+    	c=S.nextInt();
+    	return 0;
     	
 	}
 	
     public void userInputs()
     {
-    	
-    	
+    	Scanner s=new Scanner(System.in);
+        System.out.println(" Enter first number");
+        a=s.nextInt();
+        System.out.println(" Enter second number");
+        b=s.nextInt();
+     
+        
     }
 
     public void performOperation()
 {
     	
-        }
-       
-    }
+    	switch(c)
+    	{
+    	case 1: res=add();
+               System.out.println(res);
+                 break;
+    	case 2: res=subtract();
+    	          System.out.println(res);
+    	        break;
+    	case 3: res=multiply();
+    	          System.out.println(res);
+    	           break;
+    	case 4: res=divide();
+    	   System.out.println(res);
+    	        break;
+    	   
+	}
+}
+    
     
 	int add()
 	{
-		
+		c=a+b;
+		return c;
 	}
 	
 	int subtract()
 	{
+		c=a-b;
+		return c;
 		
 	}
 	
 	int multiply()
 	{
+		c=a*b;
+		return c;
 		
 	}
 	
 	int divide()
 	{
+		c=a/b;
+		return c;
+		
 	}
-	
 }
+	public class BasicCalc
+	{
+		public static void main(String args[])
+		{
+			Calc1 c =new Calc1();
+			c.menu();
+			c.userInputs();
+			c.performOperation();
+	
+
+		}
+	}
